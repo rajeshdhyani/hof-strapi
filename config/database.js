558375ec04eb027438c562cb6b59122906f7dae6
Rajesh Dhyani <rajesh.dhyani@hexagon.com>
2022-04-10 22:16:53 +0530

    Updated production environment config

--- config/database.js
+++ config/database.js
@@ -23,21 +23,21 @@
 // });
 
 // } else {
-//   module.exports = ({ env }) => ({
-//     defaultConnection: 'default',
-//     connections: {
-//       default: {
-//         connector: 'bookshelf',
-//         settings: {
-//           client: 'sqlite',
-//           filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//         },
-//         options: {
-//           useNullAsDefault: true,
-//         },
-//       },
-//     },
-//   });
+  module.exports = ({ env }) => ({
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'sqlite',
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
+      },
+    },
+  });
 // }
 
 
@@ -90,30 +90,29 @@
 //       database: config.database,
 //       user: config.user,
 //       password: config.password,
-//       ssl: env('DATABASE_SSL', false),
-//       // ssl: {
-//       //   rejectUnauthorized: false
-//       // },
+      // ssl: {
+      //   rejectUnauthorized: false
+      // },
 //     },
 //     debug: false,
 //   },
 // });
 // }
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      },
-    },
-    debug: false,
-  },
-});
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'strapi'),
+//       user: env('DATABASE_USERNAME', 'strapi'),
+//       password: env('DATABASE_PASSWORD', 'strapi'),
+//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//       ssl: {
+//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//       },
+//     },
+//     debug: false,
+//   },
+// });
