927b7ae0fd65260a693e738e8d30dec563f4fdd7
Rajesh Dhyani <rajesh.dhyani@hexagon.com>
2022-04-10 21:41:20 +0530

    Updated production environment config

--- config/database.js
+++ config/database.js
@@ -1,44 +1,44 @@
 // const parse = require('pg-connection-string').parse;
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = () => ({
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host: process.env.DATABASE_HOST,
-          port: process.env.DATABASE_PORT,
-          database: process.env.DATABASE_NAME,
-          username: process.env.DATABASE_USERNAME,
-          password: process.env.DATABASE_PASSWORD,
-          ssl: {
-            rejectUnauthorized: false
-          }
-        },
-        options: {}
-      }
-    }
-  });
+// if (process.env.NODE_ENV === 'production') {
+// module.exports = () => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "postgres",
+//         host: process.env.DATABASE_HOST,
+//         port: process.env.DATABASE_PORT,
+//         database: process.env.DATABASE_NAME,
+//         username: process.env.DATABASE_USERNAME,
+//         password: process.env.DATABASE_PASSWORD,
+//         ssl: {
+//           rejectUnauthorized: false
+//         }
+//       },
+//       options: {}
+//     }
+//   }
+// });
 
-} else {
-  module.exports = ({ env }) => ({
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
-      },
-    },
-  });
-}
+// } else {
+//   module.exports = ({ env }) => ({
+//     defaultConnection: 'default',
+//     connections: {
+//       default: {
+//         connector: 'bookshelf',
+//         settings: {
+//           client: 'sqlite',
+//           filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//         },
+//         options: {
+//           useNullAsDefault: true,
+//         },
+//       },
+//     },
+//   });
+// }
 
 
 
@@ -59,42 +59,42 @@ if (process.env.NODE_ENV === 'production') {
 //     },
 //   });
 // } else {
-  // module.exports = () => ({
-  //   defaultConnection: "default",
-  //   connections: {
-  //     default: {
-  //       connector: "bookshelf",
-  //       settings: {
-  //         client: "postgres",
-  //         host: process.env.DATABASE_HOST,
-  //         port: process.env.DATABASE_PORT,
-  //         database: process.env.DATABASE_NAME,
-  //         username: process.env.DATABASE_USERNAME,
-  //         password: process.env.DATABASE_PASSWORD,
-  //         ssl: {
-  //           rejectUnauthorized: false
-  //         }
-  //       },
-  //       options: {}
-  //     }
-  //   }
-  // });
-//   const parse = require('pg-connection-string').parse;
-//     const config = parse(process.env.DATABASE_URL);
-//     module.exports = ({ env }) => ({
-//       connection: {
-//         client: 'postgres',
-//         connection: {
-//           host: config.host,
-//           port: config.port,
-//           database: config.database,
-//           user: config.user,
-//           password: config.password,
-//           ssl: {
-//             rejectUnauthorized: false
-//           },
-//         },
-//         debug: false,
+// module.exports = () => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "postgres",
+//         host: process.env.DATABASE_HOST,
+//         port: process.env.DATABASE_PORT,
+//         database: process.env.DATABASE_NAME,
+//         username: process.env.DATABASE_USERNAME,
+//         password: process.env.DATABASE_PASSWORD,
+//         ssl: {
+//           rejectUnauthorized: false
+//         }
 //       },
-//     });
+//       options: {}
+//     }
+//   }
+// });
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+    debug: false,
+  },
+});
 // }
