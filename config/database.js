431428a2ac318b0745065025e66440f4c2b96c80
Rajesh Dhyani <rajesh.dhyani@hexagon.com>
2022-04-10 22:28:05 +0530

    Updated production environment config

--- config/database.js
+++ config/database.js
@@ -31,24 +31,43 @@
       },
     });
   } else {
-    module.exports = () => ({
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: "postgres",
-            host: process.env.DATABASE_HOST,
-            port: process.env.DATABASE_PORT,
-            database: process.env.DATABASE_NAME,
-            username: process.env.DATABASE_USERNAME,
-            password: process.env.DATABASE_PASSWORD,
-            ssl: { 
-              rejectUnauthorized: false 
-            } 
+
+    module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', 'strapi'),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
           },
-          options: {}
-        }
-      }
+        },
+        debug: false,
+      },
     });
+    
+    // module.exports = () => ({
+    //   defaultConnection: "default",
+    //   connections: {
+    //     default: {
+    //       connector: "bookshelf",
+    //       settings: {
+    //         client: "postgres",
+    //         host: process.env.DATABASE_HOST,
+    //         port: process.env.DATABASE_PORT,
+    //         database: process.env.DATABASE_NAME,
+    //         username: process.env.DATABASE_USERNAME,
+    //         password: process.env.DATABASE_PASSWORD,
+    //         ssl: { 
+    //           rejectUnauthorized: false 
+    //         } 
+    //       },
+    //       options: {}
+    //     }
+    //   }
+    // });
   }
